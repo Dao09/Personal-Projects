67bdb606db871bd3d2b5273696ec4cec1c5efce7
Eternal68 <40053533+Eternal68@users.noreply.github.com>
2021-01-13 13:25:41 +0530

    prism fixed, phaseshift added

---
+++ Web-development/Fron-end Course/Learnings/jquery/issue/jquery/script.js
@@ -0,0 +1,39 @@
+$(function(){
+
+    $(".red").fadeIn(1000);
+    $(".red").fadeOut(1000);
+    for(var x=0;x<1000;x++){
+      $(".red").fadeToggle(1000);
+      $(".blue").hide(1000);
+      $(".blue").show(100);
+      $("p").hide();
+      $("p").slideDown();
+    //   $(".green").animate({"margin-top":"50px"},1000);
+    //   $(".green").animate({"margin-top":"-=50px"},2000);
+
+      $(".green").animate({
+        marginTop: "+=50px",
+        height: "70px",
+        width:"70px",
+        "opacity":"0",
+      },2000)
+
+      $(".green").animate({
+        marginTop: "-=50px",
+        height: "150px",
+        width:"150px",
+        "opacity":"1",
+      },1000)
+
+    }
+    
+    
+    $(".box").delay(2000).fadeIn(1000);
+
+   
+
+})
+
+function hide_div(){
+    $(".box").fadeOut();
+}
